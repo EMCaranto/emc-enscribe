e3fadae448712244806d2df46e775b8f71c90adb
Erickson Caranto <erickson.caranto.m@gmail.com>
2024-01-13 20:40:26 +0800

    :construction: working on archived box component

--- src/app/( main )/_components/archive-box.tsx
+++ src/app/( main )/_components/archive-box.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 // React JS
-import React from 'react'
+import React, { useState } from 'react'
 
 // Next JS
 import { useParams, useRouter } from 'next/navigation'
@@ -22,6 +22,60 @@ import { Id } from '../../../../convex/_generated/dataModel'
 import { api } from '../../../../convex/_generated/api'
 
 const ArchivedBox = () => {
+  const [search, setSearch] = useState('')
+
+  const params = useParams()
+  const router = useRouter()
+
+  const getArchivedDoc = useQuery(api.documents.getArchivedDocument)
+  const onDeleteDoc = useMutation(api.documents.onDeleteDocument)
+  const onRestoreDoc = useMutation(api.documents.onRestoreDocument)
+
+  const filteredDoc = getArchivedDoc?.filter((archiveDoc) => {
+    return archiveDoc.title.toLowerCase().includes(search.toLowerCase())
+  })
+
+  const onClickHandler = (documentId: string) => {
+    router.push(`/documents/${documentId}`)
+  }
+
+  const onRestoreHandler = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+    documentId: Id<'documents'>
+  ) => {
+    event.stopPropagation()
+
+    const promise = onRestoreDoc({ id: documentId })
+
+    toast.promise(promise, {
+      loading: 'Restoring note...',
+      success: 'Note restored!',
+      error: 'Failed to restored note.',
+    })
+  }
+
+  const onDeleteHandler = (documentId: Id<'documents'>) => {
+    const promise = onDeleteDoc({ id: documentId })
+
+    toast.promise(promise, {
+      loading: 'Deleting note...',
+      success: 'Note deleted!',
+      error: 'Failed to delete note.',
+    })
+
+    if (params.documentId === documentId) {
+      router.push('/documents')
+    }
+  }
+
+  if (getArchivedDoc === undefined) {
+    return (
+      <div className="flex h-full items-center justify-center p-4">
+        <Spinner size={'lg'} />
+      </div>
+    )
+  }
+
   return <div>ArchivedBox</div>
 }
 
