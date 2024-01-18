3ec866e696a98bdc71ebbe64ff02b8ffcc00f7c7
Erickson Caranto <erickson.caranto.m@gmail.com>
2024-01-18 19:27:42 +0800

    :hammer: add icon picker component

---
+++ src/components/global/icon-picker.tsx
@@ -0,0 +1,50 @@
+'use client'
+
+// React JS
+import React from 'react'
+
+// Dependencies
+import EmojiPicker, { Theme } from 'emoji-picker-react'
+import { useTheme } from 'next-themes'
+
+// Components
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
+
+interface IconPickerProps {
+  children: React.ReactNode
+  onChange: (icon: string) => void
+  asChild?: boolean
+}
+
+const IconPicker = ({ children, onChange, asChild }: IconPickerProps) => {
+  const { resolvedTheme } = useTheme()
+
+  const theme = (resolvedTheme || 'light') as keyof typeof themeModeMap
+
+  const themeModeMap = {
+    light: Theme.LIGHT,
+    dark: Theme.DARK,
+  }
+
+  const currentTheme = themeModeMap[theme]
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <PopoverContent className="w-full border-none p-0 shadow-none">
+          <EmojiPicker
+            height={350}
+            theme={currentTheme}
+            onEmojiClick={(data) => onChange(data.emoji)}
+          />
+        </PopoverContent>
+      </PopoverTrigger>
+    </Popover>
+  )
+}
+
+export default IconPicker
